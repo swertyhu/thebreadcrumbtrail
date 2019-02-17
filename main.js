9b43b39d2313356ab66b5f25cbeba8f4b32cec84
burwin <burwincollege@gmail.com>
2019-02-17 02:10:24 -0800

    final cleanups for frontends

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-var button = document.getElementById('1');
+var button1 = document.getElementById('1');
+var button2 = document.getElementById('2');
+var button3 = document.getElementById('3');
 var buttonInfoText = document.createTextNode("MASS INFO HERE");
 var body = document.getElementById('main-body')
 
-button.addEventListener('click', function(){
+button1.addEventListener('click', function(){
+
+});
+button2.addEventListener('click', function(){
+
+});
+button3.addEventListener('click', function(){
+
+});
+$("1").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("main-body").offset().top
+    }, 2000);
 });
 /*button.addEventListener('mouseover', function(){
     textBox.style.display = "block";
