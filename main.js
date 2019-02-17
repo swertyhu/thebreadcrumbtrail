e0899eade96f0328a769a7042cbb2ea2ba3fd140
burwin <burwincollege@gmail.com>
2019-02-16 20:56:08 -0800

    animated

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-var button = document.getElementById('buttonMore');
+var button = document.getElementById('1');
 var buttonInfoText = document.createTextNode("MASS INFO HERE");
-var textBox = document.getElementById('textBox');
-textBox.style.display = "none";
 
 button.addEventListener('click', function(){
-    location.href = "heatmap.html";
+    window.location = "heatmap.html";
 });
-button.addEventListener('mouseover', function(){
+/*button.addEventListener('mouseover', function(){
     textBox.style.display = "block";
     //document.body.style.backgroundColor = 'green';
 });
 button.addEventListener('mouseout', function(){
     textBox.style.display = "none";
-});
+});*/
