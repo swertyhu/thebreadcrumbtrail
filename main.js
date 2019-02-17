f204d4a7af8494538cd7f3ee6497b46d0183e1c6
burwin <burwincollege@gmail.com>
2019-02-16 18:27:57 -0800

    cleaning code~

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-function mouseOver(location){
-    switch(location){
-        case 1:
-            pass
-    }
-}
+var button = document.getElementById('buttonMore');
+var buttonInfoText = document.createTextNode("MASS INFO HERE");
+var textBox = document.getElementById('textBox');
+textBox.style.display = "none";
+
+button.addEventListener('click', function(){
+    location.href = "heatmap.html";
+});
+button.addEventListener('mouseover', function(){
+    textBox.style.display = "block";
+    //document.body.style.backgroundColor = 'green';
+});
+button.addEventListener('mouseout', function(){
+    textBox.style.display = "none";
+});
